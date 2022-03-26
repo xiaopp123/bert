c78729fc1f3008708fdd23c2bc9eaec78b1a8533
zhaopei.666 <zhaopei.666@bytedance.com>
2022-03-26 18:38:39 +0800

    Add pretrian data

--- create_pretraining_data.py
+++ create_pretraining_data.py
@@ -185,14 +185,14 @@ def create_training_instances(input_files, tokenizer, max_seq_length,
     input_files: 语料文件
     tokenizer: 分词器
     max_seq_length: 句子最大长度
-    dupe_factor:
+    dupe_factor: 输入语料重复次数
     short_seq_prob:
     masked_lm_prob:
     max_predictions_per_seq:
     rng:
 
   """
-  # all_documments用来存储所有段落句子分词后结果
+  # all_documments用来存储所有文章句子分词后结果
   all_documents = [[]]
 
   # Input file format:
@@ -228,6 +228,7 @@ def create_training_instances(input_files, tokenizer, max_seq_length,
   instances = []
   for _ in range(dupe_factor):
     for document_index in range(len(all_documents)):
+      # 对每篇文章构建序列instance
       instances.extend(
         create_instances_from_document(
           all_documents,
@@ -246,6 +247,7 @@ def create_instances_from_document(
     all_documents, document_index, max_seq_length, short_seq_prob,
     masked_lm_prob, max_predictions_per_seq, vocab_words, rng):
   """Creates `TrainingInstance`s for a single document."""
+  # 当前文章每个句子分词结果，形如 [['ab', 'bc'], ['a', 'b']]
   document = all_documents[document_index]
 
   # Account for [CLS], [SEP], [SEP]
@@ -259,6 +261,8 @@ def create_instances_from_document(
   # sequences to minimize the mismatch between pre-training and fine-tuning.
   # The `target_seq_length` is just a rough target however, whereas
   # `max_seq_length` is a hard limit.
+  # 由于在fine-tunint过程中，文本有长有短，为了保持训练和fine-tuning过程一致，
+  # 训练序列并不是都是 设置的最大长度，而是以0.1的概率生成短序列
   target_seq_length = max_num_tokens
   if rng.random() < short_seq_prob:
     target_seq_length = rng.randint(2, max_num_tokens)
@@ -294,7 +298,8 @@ def create_instances_from_document(
         tokens_b = []
         # Random next
         is_random_next = False
-        # 如果当前只有一个句子或者概率小于0.5，则随机选择一个句子b
+        # 对于a_end后面的句子，0.5的概率保留，0.5的概率替换
+        # 替换：如果当前只有一个句子或者概率小于0.5，则随机选择一个句子b
         if len(current_chunk) == 1 or rng.random() < 0.5:
           is_random_next = True
           # b序列的长度
@@ -304,14 +309,14 @@ def create_instances_from_document(
           # corpora. However, just to be careful, we try to make sure that
           # the random document is not the same as the document
           # we're processing.
-          # 从非当前段落中随机选择
+          # 从其他文章中随机选择
           for _ in range(10):
             random_document_index = rng.randint(0, len(all_documents) - 1)
             if random_document_index != document_index:
               break
 
           random_document = all_documents[random_document_index]
-          # 从选择的段落中，随机选择一个起始位置
+          # 从选择的文章中，随机选择一个起始位置
           random_start = rng.randint(0, len(random_document) - 1)
           # 从选择的开始位置开始往后进行找句子，当句子总长度大于b序列的长度时停止
           for j in range(random_start, len(random_document)):
@@ -326,6 +331,7 @@ def create_instances_from_document(
           i -= num_unused_segments
         # Actual next
         else:
+          # 保留 后面的句子
           is_random_next = False
           # 序列a确定后，后面的作为b
           for j in range(a_end, len(current_chunk)):
@@ -336,6 +342,7 @@ def create_instances_from_document(
         assert len(tokens_a) >= 1
         assert len(tokens_b) >= 1
 
+        # 两个序列合并成一个序列，开头添加[CLS]，中间和后面添加[SEP]
         tokens = []
         segment_ids = []
         tokens.append("[CLS]")
@@ -382,9 +389,9 @@ def create_masked_lm_predictions(tokens, masked_lm_prob,
   """Creates the predictions for the masked LM objective.
 
   Args:
-    tokens: 序列a与序列b构建的tokens
+    tokens: 序列a与序列b合并的序列tokens
     masked_lm_prob: mask概率，默认0.15
-    max_predictions_per_seq: 单个序列中最多允许mask的token数量
+    max_predictions_per_seq: 序列中最多允许mask的token数量
     vocab_words: 词表
     rng: 随机数生成器
 
@@ -417,18 +424,19 @@ def create_masked_lm_predictions(tokens, masked_lm_prob,
 
   output_tokens = list(tokens)
 
-  # 当前序列需要mask的个数
+  # 当前序列需要mask的单词个数
   num_to_predict = min(max_predictions_per_seq,
                        max(1, int(round(len(tokens) * masked_lm_prob))))
 
   masked_lms = []
   covered_indexes = set()
-  # 从mask候选单词集合中选择num_to_predict个
+  # 从当前序列单词集合中选择num_to_predict个
   for index_set in cand_indexes:
     if len(masked_lms) >= num_to_predict:
       break
     # If adding a whole-word mask would exceed the maximum number of
     # predictions, then just skip this candidate.
+    # 如果mask当前单词（该单词有word_piece）超过最大mask个数，则跳过该单词
     if len(masked_lms) + len(index_set) > num_to_predict:
       continue
     is_any_index_covered = False
@@ -455,8 +463,10 @@ def create_masked_lm_predictions(tokens, masked_lm_prob,
         else:
           masked_token = vocab_words[rng.randint(0, len(vocab_words) - 1)]
 
+      # 当前位置替换mask后的token
       output_tokens[index] = masked_token
 
+      # mask单词结果列表，(token_index，label(真实token))
       masked_lms.append(MaskedLmInstance(index=index, label=tokens[index]))
   assert len(masked_lms) <= num_to_predict
   masked_lms = sorted(masked_lms, key=lambda x: x.index)
@@ -477,11 +487,13 @@ def truncate_seq_pair(tokens_a, tokens_b, max_num_tokens, rng):
     if total_length <= max_num_tokens:
       break
 
+    # 对较长的句子进行裁剪
     trunc_tokens = tokens_a if len(tokens_a) > len(tokens_b) else tokens_b
     assert len(trunc_tokens) >= 1
 
     # We want to sometimes truncate from the front and sometimes from the
     # back to add more randomness and avoid biases.
+    # 裁剪时，0.5概率删除序列开头字符，0.5概率删除序列后面字符
     if rng.random() < 0.5:
       del trunc_tokens[0]
     else:
@@ -507,7 +519,7 @@ def main(_):
 
   # 随机数生成器
   rng = random.Random(FLAGS.random_seed)
-  # 构建序列列表
+  # 读取原始文本文件，构建训练序列列表
   instances = create_training_instances(input_files, tokenizer, FLAGS.max_seq_length, FLAGS.dupe_factor, FLAGS.short_seq_prob, FLAGS.masked_lm_prob, FLAGS.max_predictions_per_seq, rng)
   output_files = FLAGS.output_file.split(",")
   tf.logging.info("*** Writing to output files ***")
